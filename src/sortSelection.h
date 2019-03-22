748519f2a594cde4c0cf030e5d0ff0a20fc8e6ab
t o <finalfantasytimothy@gmail.com>
2019-03-21 20:25:03 -0700

    Working on selection sort

--- src/sortSelection.h
+++ src/sortSelection.h
@@ -1,4 +1,4 @@
-/***********************************************************************
+﻿/***********************************************************************
  * Module:
  *    Week 11, Sort Select
  *    Brother Helfrich, CS 235
@@ -10,14 +10,32 @@
 
 #ifndef SORT_SELECTION_H
 #define SORT_SELECTION_H
+#include <iostream>
+#include <algorithm>
 
 /*****************************************************
  * SORT SELECTION
  * Perform the selection sort
  ****************************************************/
 template <class T>
-void sortSelection(T array[], int num)
+void sortSelection(T sArray[], int num)
 {
+	int iPivot = 0;
+	int iLargest = 0;
+	for (iPivot = num - 1; iPivot --;)
+	{
+
+		iLargest = 0;
+
+		for (int iCheck = 1; iCheck < iPivot - 1; iCheck++)
+		{		
+			if (sArray[iLargest] < sArray[iCheck])
+				iLargest = iCheck;
+
+			if (sArray[iLargest] > sArray[iPivot])
+				std::swap(sArray[iLargest], sArray[iPivot]);
+		}	
+	}
 }
 
 
