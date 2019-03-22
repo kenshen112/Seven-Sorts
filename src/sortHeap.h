3b8a69a63ed5bb4ed66aa22bdd2c0c559ce765fa
t o <finalfantasytimothy@gmail.com>
2019-03-22 11:40:53 -0700

    Start to HeapSort

--- src/sortHeap.h
+++ src/sortHeap.h
@@ -10,15 +10,71 @@
 
 #ifndef SORT_HEAP_H
 #define SORT_HEAP_H
+#include <iostream>
+#include <algorithm>
 
 /*****************************************************
  * SORT HEAP
  * Perform the heap sort
  ****************************************************/
 template <class T>
+class Heap
+{
+private:
+	T *array;
+	int num;
+public:
+	Heap()
+	{
+		array = NULL;
+		num = 0;
+	}
+
+	Heap(T arrayInput, int numInput)
+	{
+		array = arrayInput;
+		num = numInput;
+
+		for (int index = (num / 2); index < num; index++)
+		{
+			percolateDown(index);
+		}
+	}
+
+	void percolateDown(int index);
+};
+
+
+template<class T>
+void Heap<T>::percolateDown(int index)
+{
+	int indexLeft = index * 2;
+	int indexRight = indexLeft + 1;
+
+	if (indexRight < num && array[indexRight] > array[indexLeft] 
+		&& array[indexRight] > array[index])
+	{
+		std::swap(index, indexRight);
+		percolateDown(indexRight);
+	}
+
+	else if (indexLeft < num && array[indexLeft] > array[index])
+	{
+		std::swap(index, indexLeft);
+		percolateDown(indexLeft);
+	}
+}
+
+template <class T>
 void sortHeap(T array[], int num)
 {
+	Heap<T> heap;
 
+	while (num > 1)
+	{
+		num--;
+		heap.percolateDown(1);
+	}
 }
 
 
