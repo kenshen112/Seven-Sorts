147f8aa8df93a0405f3e99bcd8f90b7f3eeb1f22
CS235 Sec 5 Group 7 <cs235s5g7@LinuxLab01.byui.edu>
2019-03-25 16:29:50 -0600

    almost fixed heap

--- src/sortHeap.h
+++ src/sortHeap.h
@@ -34,7 +34,16 @@ class Heap
    void sort();
    void heapify(T *arrayInput, int numInput);
    void percolateDown(int index);
+   void swap(int t1, int t2);
 };
+
+template<class T>
+void Heap<T>::swap(int t1, int t2)
+{
+   T temp = array[t1];
+   array[t1] = array[t2];
+   array[t2] = temp;
+}
 
 
 template<class T>
@@ -43,72 +52,103 @@ T Heap<T>::getMax()
    return array[0];
 }
 
+/****************************************************
+ * delete max
+ * why does whe even have that lever?
+ *******************************************/
 template<class T>
 void Heap<T>::deleteMax()
 {
    array[0] = NULL;
 }
 
+/****************************************************
+ * SORT
+ * organizes the heavy lifting
+ ***********************************************/
 template<class T>
 void Heap<T>::sort()
 {
-	heapify(array, num);
+   while (num > 1)
+   {
+      swap(0, num);
+      num--;
+      percolateDown(0);
+   }
+   
 
-
-	for(int i = num-1; i>=0; i--)
-	{
-		std::swap(array[0], array[i]);
-		heapify(array, i);
-
-	}
+   
+   /*heapify(array, num);
+   
+   
+   for(int i = num-1; i>=0; i--)
+   {
+      std::swap(array[0], array[i]);
+      heapify(array, i);
+      
+      }*/
 }
 
+/************************************************************
+ * HEAPIFY
+ * called in the constructor is helps set up up
+ *******************************************************/
 template<class T>
 void Heap<T>::heapify(T * arrayInput, int numInput)
 {
-	array = arrayInput;
-	num = numInput;
-
-	for (int index = num / 2 - 1; index >= 0; index--)
-	{
-		percolateDown(index);
-	}
+   array = arrayInput;
+   num = numInput;
+   
+   for (int index = num / 2 - 1; index >= 0; index--)
+   {
+      percolateDown(index);
+   }
 }
 
+/**************************************************************
+ * PERCOLATE DOWN
+ * enforces the heap based structure throughout the array
+ ************************************************************/
 template<class T>
 void Heap<T>::percolateDown(int index)
 {
-	int indexLeft = index * 2 + 1;
-	int indexRight = indexLeft + 1;
-
-	if (indexRight < num && array[indexRight] > array[indexLeft] && array[indexRight] > array[index])
-	{
-		index = indexRight;
-	}
-
-	else if (indexLeft < num && array[indexLeft] > array[index])
-	{
-		index = indexLeft;
-	}
-
-	if (array[index] == array[0])
-	{
-		return;
-	}
-
-	else
-	{
-		std::swap(array[0], array[index]);
-	}
-
-	percolateDown(0);
+   int indexLeft = index * 2 + 1;
+   int indexRight = indexLeft + 1;
+   
+   if (indexRight <= num && array[indexRight] > array[indexLeft] && array[indexRight] > array[index])
+   { 
+      swap(index, indexRight);
+      percolateDown(indexRight);
+   }
+   else if (indexLeft <= num && array[indexLeft] > array[index])
+   {
+      swap(index, indexLeft);
+      percolateDown(indexLeft);
+   }
+/*   
+   if (array[index] == array[0])
+   {
+      return;
+   }
+   
+   else
+   {
+      std::swap(array[0], array[index]);
+   }
+   
+   percolateDown(0);*/
 }
 
+/********************************************
+ * SORT HEAP
+ * allows the client to interface with the heap
+ * object as if it were an ordinary algorithm
+ ******************************************/
 template <class T>
 void sortHeap(T array[], int num)
 {
-	Heap<T> heap(array, num); // heapifies
-	heap.sort();
+   Heap<T> heap(array, num); // heapifies
+   heap.sort(); //sortifies
 }
 
 
